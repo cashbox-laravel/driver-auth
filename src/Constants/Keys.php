82e9ca94f241d0ece2a5bb5f1c32694d8162444a
Andrey Helldar <helldar@ai-rus.com>
2021-08-27 20:06:20 +0300

    First Initial

---
+++ src/Constants/Keys.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace YourName\CashierDriver\BankName\Auth\Constants;
+
+class Keys
+{
+    public const TERMINAL = 'TerminalKey';
+
+    public const TOKEN = 'Token';
+
+    public const PASSWORD = 'Password';
+}
